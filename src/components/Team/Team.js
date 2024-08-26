faff0e9eed460807436452e16494da5469c529fc
patelshivam <shivampatel3650@gmail.com>
2024-08-26 12:21:58 +0530

    team in home is done

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,12 +4,22 @@ import { HoverBorderGradient } from "../ui/hover-border-gradient";
 
 function Team() {
   return (
-    <div className="w-full min-h-[100vh] pb-20 py-10 bg-black">
+    <div className="w-full min-h-[50vh] pb-20 py-10 bg-black">
       <h2 className="max-w-7xl pl-4 mx-[2em] mb-[1em] text-xl md:text-5xl font-bold text-neutral-200 font-sans">
         The Minds Behind the Magic.
       </h2>
 
-      <div className="team mx-[2em] flex flex-row flex-wrap justify-center">
+      <div className="team mx-[2em] flex-grow-0 flex-shrink-0 basis-1/3 flex flex-row flex-wrap justify-center">
+        <TeamCard
+          name="xyz"
+          position="President"
+          img="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <TeamCard
+          name="xyz"
+          position="Vice President"
+          img="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
         <TeamCard
           name="Khushi Shah"
           position="General Secretary"
@@ -18,21 +28,6 @@ function Team() {
         <TeamCard
           name="Krish Modi"
           position="Treasurer"
-          img="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <TeamCard
-          name="Shivam Patel"
-          position="Tech Head"
-          img="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <TeamCard
-          name="Ansh Soni"
-          position="GD Head"
-          img="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <TeamCard
-          name="Khushi Uttamani"
-          position="Social Media Head"
           img="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
       </div>
--- src/components/Team/TeamCard.js
+++ src/components/Team/TeamCard.js
@@ -5,7 +5,7 @@ function TeamCard({ name, position, img }) {
   return (
     <div>
       <CardContainer className="inter-var flex mx-[1em]">
-        <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2] w-auto sm:w-[20rem] min-h-[35vh] rounded-xl p-6 border">
+        <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2] w-auto sm:w-[19rem] min-h-[35vh] rounded-xl p-6 border">
           <CardItem translateZ="50" className="text-xl font-bold text-white">
             {name}
           </CardItem>
@@ -16,38 +16,26 @@ function TeamCard({ name, position, img }) {
           >
             {position}
           </CardItem>
-          <CardItem translateZ="100" className="w-full mt-4">
+          <CardItem
+            translateZ="100"
+            className="w-full flex justify-center items-center mt-4"
+          >
             <img
               src={img}
-              height="1000"
-              width="1000"
-              className="h-30 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+              className="h-[150px] w-[160px] object-cover rounded-full group-hover/card:shadow-xl"
               alt="thumbnail"
             />
           </CardItem>
 
-          <CardItem translateZ="50" className="text-xl font-bold text-white">
-            <span></span>
+          <CardItem
+            translateZ="50"
+            className="text-xl font-bold text-white mt-3"
+          >
+            <span>
+              <i className="fa-brands fa-linkedin mr-3 cursor-pointer"></i>
+              <i className="fa-brands fa-instagram cursor-pointer"></i>
+            </span>
           </CardItem>
-
-          {/* <div className="flex justify-between items-center mt-5">
-            <CardItem
-              translateZ={20}
-              // as={Link}
-              href="https://twitter.com/mannupaaji"
-              target="__blank"
-              className="px-4 py-2 rounded-xl text-xs font-normal text-white"
-            >
-              Try now →
-            </CardItem>
-            <CardItem
-              translateZ={20}
-              as="button"
-              className="px-4 py-2 rounded-xl bg-white text-black text-xs font-bold"
-            >
-              Sign up
-            </CardItem>
-          </div> */}
         </CardBody>
       </CardContainer>
     </div>
