ab1e19ed250d6e66affbc53f6152ba27279a2a1d
patelshivam <shivampatel3650@gmail.com>
2024-08-30 20:25:26 +0530

    InauguralCeremony images

--- src/components/Gallery/MainGalley.js
+++ src/components/Gallery/MainGalley.js
@@ -117,183 +117,210 @@ function Gallery() {
     { src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/EDM90.jpg" },
   ];
 
-  const InaugralCeremony = [
+  const InauguralCeremony = [
     {
-      src: "https://res.cloudinary.com/dr8kooipi/image/upload/v1724960224/SKS_6406_sadet6.jpg",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/10Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01220.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/11Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01226.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/12Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01236.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/13Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01239.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/14Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01255.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/15Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01263.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/16Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01264.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/17Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC01270.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/18Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07278.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/19Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07280.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/1Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07282.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/20Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07284.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/21Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07289.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/22Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07294.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/23Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07300.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/24Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07310.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/25Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07312.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/26Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07316.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/27Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07319.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/28Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07322.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/29Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07327.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/2Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07350.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/30Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07361.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/31Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07376.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/32Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07379.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/33Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07380.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/34Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07396.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/35Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07405.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/36Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC07430.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/37Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0007.JPG?alt=media&token=f8c790ec-3e48-4a27-a5f7-d5f6c7fe44c2",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/38Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0028.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/39Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0030.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/3Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0034.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/40Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0045.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/41Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0049.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/42Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0051.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/43Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0055.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/44Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0064.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/45Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0068.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/46Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0071.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/47Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0077.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/48Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0088.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/49Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0095.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/4Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0116.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/50Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0125.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/51Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0128.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/52Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0135.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/53Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FDSC_0141.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/54Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5399.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/55Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5410.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/56Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5488.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/57Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5521.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/58Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5557.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/59Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5602.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/5Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5618.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/60Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5627.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/61Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5688.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/62Ceremony.JPG",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/tesseract-x-0.appspot.com/o/InaugralCeremony%2FSKS_5698.JPG?alt=media&token=29850be9-ff05-4d2f-8594-4b90260ddf2b",
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/63Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/64Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/65Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/66Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/67Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/68Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/6Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/7Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/8Ceremony.JPG",
+    },
+    {
+      src: "https://snt-images-bucket.s3.ap-south-1.amazonaws.com/9Ceremony.JPG",
     },
   ];
 
@@ -403,7 +430,7 @@ function Gallery() {
               columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
             >
               <Masonry gutter="10px">
-                {InaugralCeremony.map((image, index) => (
+                {InauguralCeremony.map((image, index) => (
                   <div
                     key={index}
                     onClick={() => handleImageClick(image.src)}
