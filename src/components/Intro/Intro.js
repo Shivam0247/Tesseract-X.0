9a7fe5db7866269dd064fc6bf0dbd4520a6e4988
Shivam Patel <112711878+Shivam0247@users.noreply.github.com>
2024-10-13 14:10:39 +0530

    video link

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -293,7 +293,7 @@ function Intro() {
               className="w-[100%] h-[100%]"
             >
               <source
-                src="https://snt-images-bucket.s3.ap-south-1.amazonaws.com/ThemeLaunch.mp4"
+                src="https://snt-tess.s3.ap-south-1.amazonaws.com/ThemeLaunch.mp4"
                 type="video/mp4"
               />
             </video>
